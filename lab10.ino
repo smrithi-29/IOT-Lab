a2cb41f8e32a234b51350cadc2252d67bf2562e6
smrithi-29 <smrithiudp@gmail.com>
2025-11-10 10:32:10 +0530

    Add files via upload

---
+++ lab10.ino
@@ -0,0 +1,21 @@
+void setup()
+{
+  pinMode(8, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Serial.begin(9600);
+}
+void loop(){
+if(Serial.available())
+{
+  char data_rcvd= Serial.read();
+  if(data_crcvd=='1')
+  digitalWrite(13, HIGH);
+  if(data_rcvd=='0')
+  digitalWrite(13, LOW);
+}
+if(digitalRead(8)==HIGH)
+  Serial.write('0');
+else
+  Serial.write('1');
+}
